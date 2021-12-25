9a843f40691360f21123a16b0c9a700d6ec40ce4
klaxman27071 <klaxman2707@gmail.com>
2021-12-25 20:47:28 +0530

    New ThreeAdd1 is added

--- MyProject/src/Demo1.java
+++ MyProject/src/Demo1.java
@@ -12,6 +12,7 @@ public class Demo1 {
 		SubstrationDemo sd=new SubstrationDemo();
 		ad.add(20, 50);
 		ad.addThreenum(20, 50,50);
+		ad.addThreenum(200, 50,50);
 		sd.sub(40, 20);
 		sd.subThree(70, 20, 30);
 		sd.subThree1(70, 20, 30);
