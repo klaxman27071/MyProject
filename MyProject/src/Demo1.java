be86bc5cb34b26ee1fb2a7b216d0272641a25b13
klaxman27071 <klaxman2707@gmail.com>
2021-12-25 19:54:38 +0530

    Demo1 Class is added

---
+++ MyProject/src/Demo1.java
@@ -0,0 +1,14 @@
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		int a=20,B=20;
+		int c=0;
+		c=a+B;
+		System.out.println("Value of A and B is: "+a+" "+B);		
+		System.out.println("Result of addition is: "+c);
+		System.out.println("Completed...");
+
+	}
+
+}
